b206c39d67eb7b91fcf7f76b80294c665214841f
DmitriBrach <dmitribrach@gmail.com>
2015-06-15 13:39:02 +0300

    login page and form

--- app/views.py
+++ app/views.py
@@ -1,12 +1,27 @@
 # coding=utf-8
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remeber_me=' + str(form.remember_me.data))
+        return redirect('/index')
+
+    return render_template("login.html",
+                           title = 'Sin In',
+                           form = form,
+                           providers = app.config['OPENID_PROVIDERS'])
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' } # выдуманный пользователь
-    posts = [ # список выдуманных постов
+    user = { 'nickname': 'Miguel' }  # выдуманный пользователь
+    posts = [  # список выдуманных постов
         {
             'author': { 'nickname': 'John' },
             'body': 'Beautiful day in Portland!'
